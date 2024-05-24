cb27edab838d0995e342f3dfbcdb7c68c98805c5
Luis Rodriguez-Rivera <96503213+aluisrodriguezr@users.noreply.github.com>
2024-05-24 11:59:50 -0400

    Login.php update
    Found an potential issue with my hashed password method. Here should be the fixed Login.php

---
+++ LAMPAPI/Login.php
@@ -0,0 +1,64 @@
+
+<?php
+
+	$inData = getRequestInfo();
+	
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		// Modified to search for Hashed Password and not plain password
+		$stmt = $conn->prepare("SELECT ID, firstName, lastName, Password FROM Users WHERE Login=?");
+		$stmt->bind_param("s", $inData["login"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if( $row = $result->fetch_assoc()  )
+		{
+			// password_verify() compares hash password in database with user-provided password
+			if (password_verify($inData["password"], $row["Password"])) {
+				returnWithInfo($row["firstName"], $row["lastName"], $row['ID']);
+			} 
+			else {
+				returnWithError("Invalid Password");
+			} 
+		} 
+		else {
+			returnWithError("No Records Found");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
